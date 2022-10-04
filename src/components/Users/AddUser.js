033734f3a87fcd11df5efb1402923d87f74dd312
sungWook.jun <pkjsu3818@naver.com>
2022-10-04 22:04:38 +0900

    리액트 연습 모듈2

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -9,7 +9,7 @@ const AddUser = (props) => {
     <form onSubmit={addUserHandler}>
       <label htmlFor="username">Username</label>
       <input id="username" type="text" />
-      <label htmlFor="age">Age (Years)</label>
+      <label htmlFor="age">Age(Years)</label>
       <input id="age" type="number" />
       <button type="submit">Add User</button>
     </form>
